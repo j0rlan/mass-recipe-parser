db1375c841d9263e9a5032aae71476cb49e6f947
Jordan Bleck <jordan.bleck@gmail.com>
2019-09-27 09:24:41 -0700

    multisheet list creation enabled through additive dictionaries

--- worksheet_parser.py
+++ worksheet_parser.py
@@ -3,13 +3,37 @@
 # for parsing an xlsx document of multiple recipes and creating a csv file containing the shopping list
 
 import pandas as pd
-from sys import argv
 
 def main():
-   fn = input("input file name: ")
+   #fn = input("input file name: ")
+   fn = "food.xlsx"
    xls = pd.ExcelFile(fn)
+   shoppingList = {}
 
-   print(xls.sheet_names)
+   print("processing sheets:")
+   for sheet in xls.sheet_names:
+      #print(sheet)
+      df = xls.parse(sheet, skiprows=6)
+      rows = df.iterrows()
+      while True:
+         try:
+            ser = next(rows)[1]
+         except StopIteration:
+            break
+         try:
+            if type(ser[0]) != str:
+               continue
+            if ser[0] not in shoppingList:
+               shoppingList[ser[0]] = {ser[4]: ser[3]}
+            elif ser[4] not in shoppingList[ser[0]]:
+               shoppingList[ser[0]][ser[4]] = ser[3]
+            else:
+               shoppingList[ser[0]][ser[4]] += ser[3]
+         except:
+            continue
+
+   for line in shoppingList:
+      print(line, shoppingList[line])
 
 
 
